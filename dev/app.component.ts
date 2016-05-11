9414df5e7ed5a4019bb0213a17ab2a2036c93d5d
Pramod Nair <pramod.nair@open.edu.au>
2016-05-11 16:31:16 +1000

    Components WIP

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,11 +1,29 @@
 import {Component} from 'angular2/core';
+import {RecipesComponent} from "./recipe-book/recipes.component";
+import {RouteConfig} from "angular2/router";
+import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
 
 @Component({
     selector: 'app',
     template: `
-       
+       <header>
+            <nav>
+                <ul>
+                    <li><a>Recipes</a></li>
+                    <li><a>Shopping List</a></li>
+                </ul>
+            </nav>
+       </header>
+       <div class="main">
+            <my-recipes></my-recipes>
+       </div>
     `,
+    directives: [RecipesComponent, ShoppingListComponent]
 })
+@RouteConfig([
+    {path: '/recipes', name: 'Recipes', component: RecipesComponent, useAsDefault: true},
+    {path: '/shopping-list', name: 'ShoppingList', component: ShoppingListComponent }
+])
 export class AppComponent {
 
 }
---
+++ dev/recipe-book/recipes.component.ts
@@ -0,0 +1,17 @@
+import {Component} from "angular2/core";
+
+@Component({
+	selector: 'my-recipes',
+	template: `
+		<div class="master list">
+			Recipe Master
+		</div>
+		<div class="detail">
+			Recipe Detail
+		</div>
+		
+	`
+})
+export class RecipesComponent {
+
+}
